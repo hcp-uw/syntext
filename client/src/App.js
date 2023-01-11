501b91b46114fa2403734157f3ca04a6fc45088f
elimelt <elimelt@uw.edu>
2023-01-10 22:14:45 -0800

    added cors and a little bit of functionality to the front end with the new devapi

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,34 @@
+import { useState, useEffect} from "react" 
+import x from './services/examplecode'
+
+const Display = (props) => {
+
+  return (
+    <div>
+      {props.lines.map((line, i) => {
+        <>
+          line
+        </>
+      })}
+    </div>
+  )
+} 
+
+
 const App = () => {
+  const [display, setDisplay] = useState([])
+
+  useEffect(() => {
+    x.getEx('short')
+      .then(r => {
+        setDisplay(r.data)
+      })
+  }, [])
+
   return (
     <div>
       <p>we are all done with set up!</p>
+      
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 
 app.get('/devapi/ex/short', (req, res) => {
     res.send({
