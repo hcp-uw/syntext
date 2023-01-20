c373af27f813de622fa36c7f96a64143de7b6da2
kaitinderr <ktinder2003@gmail.com>
2023-01-20 14:03:30 -0800

    long object

--- server/server.js
+++ server/server.js
@@ -63,7 +63,56 @@ const snippetData = {
         ]  
         }  
     ],
-    long: []
+    long: [
+        {
+        id: 1, 
+        SnippetType:'FOR_LOOP',
+        length: 'LONG',
+        data: ['int j = 20;',  
+                'for (int i = 0; i < 10; i++) {',  
+                '\tSystem.out.print(j + “ - ” + i);', 
+                '\tif (i > j)',   
+                '\t\tSystem.out.println(“ < 0”)',  
+                '\telse',  
+                '\t\tSystem.out.println(“ > 0”)',  
+                '\tj -= 1;',  
+                '}' 
+            ]
+        },
+        {
+        id: 2, 
+        SnippetType:'WHILE_LOOP',
+        length: 'LONG',
+        data: ['int total = 0;',
+                'int turns = 0;',
+                'while (total < max) {',
+                '\tint amount = r.nextInt(6) + 1;',
+                '\ttotal += amount;',
+                '\tturns++',
+                '\tSystem.out.println("Day " + turns + ": $" + amount + " earned");',
+                '}',
+                'System.out.println("You earned $" + total + "!");'
+        ]
+        },
+        {
+        id: 3, 
+        SnippetType:'WHILE_LOOP',
+        length: 'LONG',
+        data: [
+            'while (input.hasNextLine()) {',
+            '\tString line = input.nextLine();',
+            '\tScanner words = new Scanner(line);',
+            '\tif (words.hasNext()) {',
+            '\t\tSystem.out.print(words.next());',
+            '\t\twhile (words.hasNext()) {',
+            '\t\t\tSystem.out.print(" " + words.next());',
+            '\t\t}',
+            '\t}',
+            '\tSystem.out.println();',
+            '}'
+        ]
+        }
+    ]
 }
 
 app.get('/devapi/ex/short', (req, res) => {
