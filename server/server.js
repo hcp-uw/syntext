86190deb8010e12aa8d82ba86903cb29834d06a3
kaitinderr <ktinder2003@gmail.com>
2023-01-20 13:49:38 -0800

    medium object

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const snippetData = {
         SnippetType: 'PRINT',
         length: 'SHORT',
         data: ['String myCat = "Matilda";',
-            'System.out.println(myCat.charAt(3));'
+                'System.out.println(myCat.charAt(3));'
         ] 
         },
         {
@@ -25,35 +25,70 @@ const snippetData = {
         SnippetType: 'PRINT',
         length: 'SHORT',
         data: ['int sum = myCat.length() - myGod.length();',
-            'System.out.println(sum);'
+                'System.out.println(sum);'
         ]
         }    
     ],
-    medium: [],
+    medium: [
+        {
+        id: 1, 
+        SnippetType:'FOR_LOOP',
+        length: 'MED',
+        data: ['for (int i = 0; i < 10; i++) {',
+                '\tSystem.out.println(i);',
+                '}'    
+        ]
+        },
+        {
+        id: 2, 
+        SnippetType:'FOR_LOOP',
+        length: 'MED',  
+        data: ['for (int i = 0; i < rows; i++) {',
+                '\tfor (int j = 0; j < columns; j++) {',
+                '\t\tSystem.out.print("[" + i + "," + j + "] ");',
+                '\t}',
+                '}'
+        ]
+        },
+        {
+        id: 3, 
+        SnippetType:'CONDITIONAL',
+        length: 'MED',
+        data: ['if (hours > 8) {',
+                '\treturn (rate * 8) + ((hours - 8) * (rate * 1.5));',
+                '}',
+                'else {',
+                '\treturn rate * hours;',
+                '}'
+        ]  
+        }  
+    ],
     long: []
 }
 
 app.get('/devapi/ex/short', (req, res) => {
-    const idNum = Math.floor(Math.random * snippetData.short.length)
-    res.send({
+    let idNum = Math.floor(Math.random * snippetData.short.length)
+    res.send(
         snippetData.short[idNum].data
         // id: 1, 
         // SnippetType:'PRINT',
         // length: 'SHORT',
         // data: ['System.out.println("goodbye world");']
-    })
+    )
 })
 
 app.get('/devapi/ex/medium', (req, res) => {
-    res.send({
-        id: 2, 
-        SnippetType:'FOR_LOOP',
-        length: 'MED',
-        data: ['for (int i = 0; i < 10; i++) {',
-            '\tSystem.out.println(i);',
-            '}'    
-        ]
-    })
+    let idNum = Math.floor(Math.random * snippetData.medium.length)
+    res.send(
+        snippetData.medium[idNum].data
+        // id: 2, 
+        // SnippetType:'FOR_LOOP',
+        // length: 'MED',
+        // data: ['for (int i = 0; i < 10; i++) {',
+        //     '\tSystem.out.println(i);',
+        //     '}'    
+        // ]
+    )
 })
 
 app.get('/devapi/ex/long', (req, res) => {
