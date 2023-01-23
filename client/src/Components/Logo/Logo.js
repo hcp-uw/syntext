633421612f5036db1c495ef6191e7fe78715c2c9
William Eng <1williameng@gmail.com>
2023-01-22 22:27:39 -0800

    added to components folder

---
+++ client/src/Components/Logo/Logo.js
@@ -0,0 +1,17 @@
+const textstyles = {
+  color: "white",
+  fontSize: "12",
+  fontFamily: "Roboto Mono",
+}
+
+const Logo = () => {
+    return(
+      <div>
+        <h1 style={textstyles} fontSize="25"><a style="" href="/">Synte&gt;&lt;t</a></h1>
+        <div className="rectangle" />
+      </div>
+    )
+  }
+
+
+export default Logo;
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import GameMode from './Components/GameMode/GameMode';
+import IconNavBar from './Components/IconNavBar/IconNavBar';
+import Logo from './Components/Logo/Logo';
+import Methods from './Components/Methods/Methods';
+// import Timer from './Timer/Timer'; 
 import App from './App';
 import styles from './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <Logo />
 );
