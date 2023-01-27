c5c033851925829d04f85dfdee2e30ebf7ecfc47
William Eng <1williameng@gmail.com>
2023-01-26 17:05:03 -0800

    sriya and salma and william made logo clickable

--- client/src/Components/Logo/Logo.js
+++ client/src/Components/Logo/Logo.js
@@ -1,21 +1,34 @@
+import styles from "./Logo.css";
+//import React, { useState } from 'react';
+
+
 const textstyles = {
   color: "white",
   fontSize: "12",
   fontFamily: "Roboto Mono",
 }
 
+
 const Logo = () => {
     return(
-      <div>
+      <div className="hoverable">
         <span><h1 style={textstyles} fontSize="25" onClick={handleClick}>Synte&gt;&lt;t</h1></span>
-        <div className="rectangle" />
       </div>
+      //onMouseOver={changedText} onMouseLeave={normalText}
     )
   }
 
   function handleClick() {
     window.location.assign('/');
   }
+
+ 
+  // function normalText() {
+  //   this.setState({text : "Syntext"}) 
+  // } 
+  // function changedText() {
+  //   this.setState({text : "Synte- -t"}) 
+  // } 
 
 
 export default Logo;
