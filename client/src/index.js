4888a7945667e1abdacd3c22770a1fc1c2605e65
William Eng <1williameng@gmail.com>
2023-01-29 18:10:27 -0800

    honestly idk but used some react-bootstrap compone

--- client/src/index.js
+++ client/src/index.js
@@ -12,23 +12,45 @@ import styles from './index.css';
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Stack from "react-bootstrap/Stack";
+import Navbar from 'react-bootstrap/Navbar';
 import "bootstrap/dist/css/bootstrap-grid.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <body style={styles}>
-      <Container>
-        <Row></Row>
+      <Container fluid>
         <Row>
-            <Col xl={12}>
-              <Logo />
+          <Navbar>
+            <Container fluid>
+              <Navbar.Brand style={{fontSize: "2.5em"}} href="/">Synte&gt;&lt;t</Navbar.Brand>
+              <Navbar.Text>profile</Navbar.Text>
+            </Container>
+          </Navbar>
+          {/* <Stack direction="horizontal" gap={2}>
+            <Col md="auto">
+              <div className='ms-auto logo' style={{padding: 10}}><Logo /></div>
             </Col>
+            <Col md={{offset: 9}}>profile</Col>
+          </Stack> */}
+          
+        </Row>
+        <Row>
+          <Col md={{ span: 3, offset: 2}}>Random Ahh Test</Col>
+          <Col sm={2}>Col 2</Col>
+          <Col sm={2}>Col 3</Col>
+        </Row>
+        <Row>
         </Row>
         <Row>
           <Language />
           <Methods />
-          <GameMode />
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <Col><GameMode/></Col>
         </Row>
       </Container>
     </body>
