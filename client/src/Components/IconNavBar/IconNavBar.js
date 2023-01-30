2f4e9777343f2664bc55c3442b37d7603aa81173
William Eng <1williameng@gmail.com>
2023-01-29 18:10:05 -0800

    made a navbar
    Co-authored-by: Sriya Bulusu <sriyabulusu@users.noreply.github.com>

--- client/src/Components/IconNavBar/IconNavBar.js
+++ client/src/Components/IconNavBar/IconNavBar.js
@@ -1,7 +1,7 @@
 const Iconnavbar = () => {
     return(
       <ul>
-        <li><a><img href="???profile logo"/></a></li>
+        <li><a><img href="???profile logo"/>profile</a></li>
         <li><a><img href="???settings logo"/></a></li>
         <li><a><img href="???leaderboard logo"/></a></li>
       </ul>
