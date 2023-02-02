f102e04a26ad1f3e5315ab8692dee4d7920b88c8
elimelt <elimelt@uw.edu>
2023-02-02 11:30:22 -0800

    added tabs to text-area

--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,27 @@ import TextArea from './components/TextArea';
 import TextArea1 from './components/TextArea1'
 
 const App = () => {
-  const example =  {
-    id: 1,
-    SnippetType:'FOR_LOOP',
-    length: 'LONG',
-    data: ['int j = 20;',
-            'for (int i = 0; i < 10; i++) {',
-            'System.out.print(j + “ - ” + i);',
-            'if (i > j)',
-            'System.out.println(“ < 0”)',
-            'else',
-            'System.out.println(“ > 0”)',
-            'j -= 1;',
-            '}'
-        ]
+	const example =  {
+		id: 1,
+		SnippetType:'FOR_LOOP',
+		length: 'LONG',
+		data: ['int j = 20;',
+						'for (int i = 0; i < 10; i++) {',
+						'	System.out.print(j + " - " + i);',
+						'	if (i > j)',
+						'		System.out.println(" < 0")',
+						'	else',
+						'		System.out.println(" > 0")',
+						'	j -= 1;',
+						'}'
+				]
 }
 
-  return (
-    <div>
-      <TextArea1 lines={example.data}/>
-    </div>
-  );
+	return (
+		<div>
+			<TextArea1 lines={example.data}/>
+		</div>
+	);
 }
 
 export default App;
