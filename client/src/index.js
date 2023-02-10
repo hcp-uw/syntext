76ff741e17cf4cd05e7266aebeafbb459a9e7104
William Eng <1williameng@gmail.com>
2023-02-09 17:29:03 -0800

    changed font sizes

--- client/src/index.js
+++ client/src/index.js
@@ -25,7 +25,7 @@ root.render(
         <Row>
           <Navbar>
             <Container fluid>
-              <Navbar.Brand style={{fontSize: "3.3em", color: "rgba(125, 97, 95, 1)", paddingLeft: 12}} href="/">Synte&gt;&lt;t</Navbar.Brand>
+              <Navbar.Brand style={{fontSize: "2.5em", color: "rgba(125, 97, 95, 1)", paddingLeft: 12}} href="/">Synte&gt;&lt;t</Navbar.Brand>
               <Navbar.Text style={{color: "white"}}><IconNavBar /></Navbar.Text>
             </Container>
           </Navbar>
@@ -40,25 +40,25 @@ root.render(
 
         {/* <Container> */}
           <Row>
-            <Col md={{span: 1, offset: 0}} style={{fontSize: "2em", color: "rgba(180, 148, 141, 1)", paddingLeft: 110, paddingTop: 120}}>1</Col>
-            <Col md={{span: 7}} style={{fontSize: "1.8em", color: "rgba(125, 97, 95, 1)", paddingLeft: 90, paddingTop: 120, whiteSpace: "no-wrap"}}>public static void main (String[] args) &#123;</Col>
+            <Col md={{span: 1, offset: 0}} style={{fontSize: "1.8em", color: "rgba(180, 148, 141, 1)", paddingLeft: 110, paddingTop: 120}}>1</Col>
+            <Col md={{span: 7}} style={{fontSize: "1.6em", color: "rgba(125, 97, 95, 1)", paddingLeft: 90, paddingTop: 120, whiteSpace: "no-wrap"}}>public static void main (String[] args) &#123;</Col>
             <Col md={{offset: 1}} style={{fontSize: "1.8em", color: "rgba(125, 97, 95, 1)", paddingTop: 40, paddingLeft: "12%"}}>0:20</Col>
           </Row>
         {/* </Container> */}
 
         <Row>
           <Col md={{span: 1, offset: 0}} style={{fontSize: "2em", color: "rgba(180, 148, 141, 1)", paddingLeft: 110}}>2</Col>
-          <Col style={{fontSize: "1.8em", color: "rgba(209, 145, 143, 1)", paddingLeft: 150}}>Scanner console = new Scanner(System.in);</Col>
+          <Col style={{fontSize: "1.6em", color: "rgba(209, 145, 143, 1)", paddingLeft: 150}}>Scanner console = new Scanner(System.in);</Col>
         </Row>
 
         <Row>
           <Col md={{span: 1, offset: 0}} style={{fontSize: "2em", color: "rgba(180, 148, 141, 1)", paddingLeft: 110}}>3</Col>
-          <Col style={{fontSize: "1.8em", color: "rgba(209, 145, 143, 1)", paddingLeft: 150}}>int numPrints = console.nextInt();</Col>
+          <Col style={{fontSize: "1.6em", color: "rgba(209, 145, 143, 1)", paddingLeft: 150}}>int numPrints = console.nextInt();</Col>
         </Row>
 
         <Row>
           <Col md={{span: 1, offset: 0}} style={{fontSize: "2em", color: "rgba(180, 148, 141, 1)", paddingLeft: 110}}>4</Col>
-          <Col style={{fontSize: "1.8em", color: "rgba(209, 145, 143, 1)", paddingLeft: 150}}> hello guys</Col>
+          <Col style={{fontSize: "1.6em", color: "rgba(209, 145, 143, 1)", paddingLeft: 150}}> hello guys</Col>
         </Row>
 
         <Row>
