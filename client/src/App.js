7c62f044ca3e0ef6ea49edcb78977d8c25620242
William Eng <1williameng@gmail.com>
2023-02-14 18:42:40 -0800

    toggledbutton
    Co-authored-by: Salma Hajian <PenguinTamer101@users.noreply.github.com>
    Co-authored-by: Sriya Bulusu <sriyabulusu@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ const App = () => {
 			<Row>
 			<Navbar>
 				<Container fluid>
-				<Navbar.Brand style={{fontSize: "2.5em", color: "rgba(125, 97, 95, 1)", paddingLeft: 12}} href="/">Synte&gt;&lt;t</Navbar.Brand>
+				<Navbar.Brand className="logo" style={{fontSize: "2.5em", color: "rgba(125, 97, 95, 1)", paddingLeft: 12}} href="/">Synte&gt;&lt;t</Navbar.Brand>
 				<Navbar.Text style={{color: "white"}}><IconNavBar /></Navbar.Text>
 				</Container>
 			</Navbar>
