8f749f3fe0cbfe2f46b6360adff2032d5fafaf15
sriya bulusu <sriya.bulusu@gmail.com>
2023-02-14 19:05:01 -0800

    made a gameoptions folder and file to organize

---
+++ client/src/Components/GameOptions/GameOptions.js
@@ -0,0 +1,64 @@
+import Language from '../Language/Language.js'
+import Methods from '../Methods/Methods.js'
+import GameMode from '../GameMode/GameMode.js'
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GameMode from './Components/GameMode/GameMode';
+import IconNavBar from './Components/IconNavBar/IconNavBar';
+import Language from './Components/Language/Language';
+import Logo from './Components/Logo/Logo';
+import Methods from './Components/Methods/Methods';
+import RestartButton from './Components/RestartButton/RestartButton';
+import Timer from './Components/Timer/Timer'; 
+import './index.css';
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Stack from "react-bootstrap/Stack";
+import Navbar from 'react-bootstrap/Navbar';
+import "bootstrap/dist/css/bootstrap-grid.min.css";
+
+const GameOptions = () => {
+	return(
+		<>
+			<body></body>
+			style={{position: "sticky"}}
+			<Row>
+			<Col>
+			<div style={{
+				color: "rgba(75, 75, 75, 1)",
+				display: 'flex',
+				paddingTop: 40,
+				alignItems: 'center',
+				justifyContent: 'center',
+				}}>
+				ctrl + n to skip
+				</div>
+			</Col>
+			</Row>
+
+			<Row>
+			<Col>
+			<div style={{
+				color: "rgba(75, 75, 75, 1)",
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+				}}>
+				ctrl + r to restart
+				</div>
+			</Col>
+			</Row>
+
+			<Row>
+			<Col md={{offset: 1, span: 1}} style={{paddingTop: 20}}>language</Col>
+			<Col md={{offset: 2, span: 3}} style={{paddingTop: 20, paddingLeft: 50}}><Methods /></Col>
+			<Col md={{offset: 2, span: 1}} style={{paddingTop: 20}}>mode</Col>
+			</Row>
+        </>
+    )
+}
+
+export default GameMode;
