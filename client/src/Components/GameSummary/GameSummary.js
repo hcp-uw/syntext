381bde472c99b77799979d9bddbfb54d8071c603
rasmus1 <rasmus1@uw.edu>
2023-02-22 14:16:52 -0800

    Added a basic template chart for displaying the game summary using Chart.js

--- client/src/Components/GameSummary/GameSummary.js
+++ client/src/Components/GameSummary/GameSummary.js
@@ -1,10 +1,12 @@
-import stylesheet from './GameSummary.css';
+import React from 'react';
+import Test from './Chart.js';
+import './GameSummary.css';
 
 const GameSummary = () => {
   return (
     <>
       <div className='game-summary'>
-        
+      <Test />
       </div>
     </>
   )
