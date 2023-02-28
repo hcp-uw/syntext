472f8e952999726c04b02b50812b514f606ea61b
PenguinTamer101 <shajian@uw.edu>
2023-02-27 16:00:56 -0800

    added index var to avoid dupllicate snippets

--- client/src/services/snippetService.js
+++ client/src/services/snippetService.js
@@ -1,15 +1,20 @@
 import axios from 'axios'
 
 const baseURL = 'http://localhost:3001'
+let previousIndex = -1; // initialize the previous index to an invalid value
 
-export default function getSnippet (len, type) {
+export default function getSnippet(len, type) {
     let snippet;
-    const req = axios
-        .get(`${baseURL}/api/read/get/lengthandtype?length=${len}&type=${type}`)
+    const req = axios.get(`${baseURL}/api/read/get/lengthandtype?length=${len}&type=${type}`);
     return req.then(res => {
-        const index = Math.floor(Math.random() * res.data.length)
-        return res.data[index].data
+        let index = Math.floor(Math.random() * res.data.length);
+        // generate a new index if it's the same as the previous index
+        while (index === previousIndex) {
+            index = Math.floor(Math.random() * res.data.length);
+        }
+        previousIndex = index; // update the previous index
+        return res.data[index].data;
     });
-
 }
+
 
