3d19457d7094235e53eecf50562eb08497d3a694
Rasmus <rasmus.makiniemi@gmail.com>
2023-03-20 11:54:17 -0700

    merged cursor functionality over from game-summary

--- client/src/components/Cursor/Cursor.js
+++ client/src/components/Cursor/Cursor.js
@@ -2,37 +2,34 @@ import stylesheet from "./Cursor.css"
 import React, { useState, useEffect } from 'react';
 
 const Cursor = (props) => {
-  const { userInput, currWord } = props;
+  const { userInput, currWord, typingStatus } = props;
+
+  useEffect(()=> {
+    moveCursor(document.querySelector('.active').querySelector('div').getBoundingClientRect(), true);
+  }, []);
 
   useEffect(() => {
-    //console.log('inside cursor effect')
-    
     return () => {
       let activeWord = document.querySelector('.active');
       let letter = document.querySelector('.cursorPos');
-
-      //console.log('activeWord: ', activeWord);
-      //console.log('letter: ', letter);
-
       if (activeWord === null) {
         return;
       } else if (letter !== null) {
         moveCursor(letter.getBoundingClientRect());
-        //console.log('letter position: ', letter.getBoundingClientRect());
       } else if (letter === null) {
         moveCursor(activeWord.querySelector('div').getBoundingClientRect(), true);
       }
     }
   }, [userInput, currWord])
 
-  function moveCursor (position, check) {
+  function moveCursor (position, newLineCheck) {
     let cursorEl = document.querySelector('.cursor');
     resetCursorBlinkAnimation(cursorEl);
 
     if (document.querySelector('.cursorPos') !== null && document.querySelector('.cursorPos').innerHTML === '	') { // We are after a tab character
       cursorEl.style.left = (position.left + 34) + 'px';
       cursorEl.style.top = (position.top - 2.5) + 'px';
-    } else if (!check) { // Normal letter
+    } else if (!newLineCheck) { // Normal letter
       cursorEl.style.left = (position.left + 10) + 'px';
       cursorEl.style.top = (position.top - 2.5) + 'px';
     } else { // New Line
@@ -47,7 +44,14 @@ const Cursor = (props) => {
     cursor.style.animation = null;
   }
 
-  return <div className={"cursor"}></div>
+  // if (document.querySelector("input") === null) {
+  //   let activeWord = document.querySelector('.word');
+  //   activeWord.querySelector('div').getBoundingClientRect();
+  // }
+
+  return (typingStatus) ? (<div className={"cursor"}></div>)
+  :
+  (<div className={"cursor hidden"}></div>);
 }
 
 export default Cursor;
