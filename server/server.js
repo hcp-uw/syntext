ed9954919b99d30933f1e5789cf7ddb72d04b9e2
elimelt <elimelt@uw.edu>
2023-04-02 16:48:25 -0700

    removed files that werent needed

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const app = require('./app');
 const config = require('./utils/config');
 
+console.table(config)
+
 app.listen(config.PORT, () => {
   console.log(`Server running on port test ${config.PORT}`);
 });
