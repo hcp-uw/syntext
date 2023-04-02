03ce473ae054624311a5f79a4d88f7487db32841
kaitinderr <ktinder2003@gmail.com>
2023-04-02 12:31:32 -0700

    completed snippet processing script

--- server/example_data/processSnippet.js
+++ server/example_data/processSnippet.js
@@ -1,11 +1,57 @@
 const fs = require('fs');
 
+const { toAscii } = require('../db/betweenASCIIValues.js')
+
 const directoryPath = './server/example_data/snippets'
 
-
+// length standard:
+// short: 1-3
+// medium: 4-7
+// long: 8-11
 
 
 // const text = fs.readFileSync(filePath, {encoding:'utf8', flag:'r'});
+
+const getType = (filePath) => {
+    const temp = filePath.split('/') // ['server', 'example_data', 'snippets', 'FOR_6.txt']
+    return temp[temp.length - 1].split('_')[0]; //['FOR', '6.txt']
+}
+
+// server/example_data/snippets/FOR_6.txt
+
+const getID = (filePath) => {
+    const type = getType(filePath)
+
+    const temp = filePath.split('/') // ['server', 'example_data', 'snippets', 'FOR_6.txt']
+    const num = temp[temp.length - 1].split('_')[1];
+    
+    let id;
+    switch (type) {
+        case "FOR": 
+            id = `1${num}` 
+            break;
+        case "METHOD":
+            id = `2${num}` 
+            break;
+        case "PRINT":
+            id = `3${num}` 
+            break;
+
+        case "WHILE":
+            id = `4${num}` 
+            break;
+    }
+    return Number(id.replace('.txt', ''));
+}
+
+
+//fileData: String[]
+const getLength = (fileData) => {
+   const len = fileData.length
+   if (len <= 3) return "SHORT";
+   if (len <= 7) return "MEDIUM";
+   else return "LONG";
+}
 
 const fileNames = [];
 
@@ -17,13 +63,24 @@ fs.readdirSync(directoryPath).forEach(file => {
 const data = []
 
 fileNames.forEach((filePath) => {
-    data.push(fs.readFileSync(filePath, {encoding:'utf8', flag:'r'})
+    data.push({
+        fileData: fs.readFileSync(filePath, {encoding:'utf8', flag:'r'})
         .replaceAll('\r', '')
-        .split('\n')
-    );
+        .split('\n'),
+        filePath: filePath
+    })
 })
 
-console.log(data);
+const pData = data.map(({ fileData, filePath }) => {
+    return {
+        id: getID(filePath),
+        type: getType(filePath),
+        length: getLength(fileData),
+        data: fileData.map(line => toAscii(line))
+    }
+});
+
+console.log(pData);
 
 
 
@@ -32,12 +89,12 @@ console.log(data);
 //     id : int,
 //     type : String,
 //     length : String,
-//     data : int[][]
-// }
+//     data : int[][] 
+//}
 
 // const method = (textFile) => {
 //     const result = Object.create(snippet)
 // }
 
 
-///server/example_data/processSnippet.js
+///server/example_data/processSnippet.js.
