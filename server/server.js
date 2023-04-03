3bb6420ba2f9364c4854bb729ae597192b11911c
elimelt <elimelt@uw.edu>
2023-04-02 17:13:36 -0700

    empty commit

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const app = require('./app');
 const config = require('./utils/config');
 
-console.table(config)
+console.table(config);
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port test ${config.PORT}`);
 });
+
 
