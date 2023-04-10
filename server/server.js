f460f4732747111ed02283e364482620cb041c55
elimelt <elimelt@uw.edu>
2023-04-10 05:08:33 -0700

    added config console table for local development

--- server/server.js
+++ server/server.js
@@ -1,11 +1,8 @@
 const app = require('./app');
 const config = require('./utils/config');
 
-<<<<<<< snippet-processing
-=======
 console.table(config)
 
->>>>>>> main
 app.listen(config.PORT, () => {
   console.log(`Server running on port test ${config.PORT}`);
 });
