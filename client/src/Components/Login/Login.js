5a641b361e02fab4a8caedbbd651187bfc4de92d
elimelt <elimelt@uw.edu>
2023-04-15 21:50:02 -0700

    created outline code for user controller/router in backend, as well as comments for implementation spec

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,55 +1,54 @@
 import { useState } from 'react';
 
-
-
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-  let users = [
-    {username: 'elijah', password: 'elijahiscool'},
-    {username: 'bob', password: 'bobiscool'}
+    let users = [
+        {username: 'elijah', password: 'elijahiscool'},
+        {username: 'bob', password: 'bobiscool'}
     ];
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
 
-  const handleLogin = (event) => {
-    event.preventDefault();
-    const user = users.find(u => u.username === username && u.password === password);
-    if (user) {
-      alert('logged in as ' + user.username);
-    } else {
-      alert('Invalid username or password');
-    }
-  };
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const user = users.find(u => u.username === username && u.password === password);
+        if (user) {
+        alert('logged in as ' + user.username);
+        } else {
+        alert('Invalid username or password');
+        }
+        // authenticate with user service
+    };
 
-  const handleCreateAccount = (event) => {
-    event.preventDefault();
-    alert('Creating account for ' + username);
-    users.push({username: username, password: password})
-    // Code to create account goes here
-  };
+    const handleCreateAccount = (event) => {
+        event.preventDefault();
+        alert('Creating account for ' + username);
+        users.push({username: username, password: password})
+        // create account with user service
+    };
 
-  return (
-    <form>
-      <div>
-        <label>Username:</label>
-        <input type="text" id="username" value={username} onChange={handleUsernameChange} />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input type="password" id="password" value={password} onChange={handlePasswordChange} />
-      </div>
-      <button type="submit" onClick={handleLogin}>Login</button>
-      <button type="submit" onClick={handleCreateAccount}>Create Account</button>
-    </form>
-  );
+    return (
+        <form>
+        <div>
+            <label>Username:</label>
+            <input type="text" id="username" value={username} onChange={handleUsernameChange} />
+        </div>
+        <div>
+            <label>Password:</label>
+            <input type="password" id="password" value={password} onChange={handlePasswordChange} />
+        </div>
+        <button type="submit" onClick={handleLogin}>Login</button>
+        <button type="submit" onClick={handleCreateAccount}>Create Account</button>
+        </form>
+    );
 };
 
 export default Login;
---
+++ server/controllers/users.js
@@ -0,0 +1,58 @@
+const usersRouter = require('express').Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+// create new user
+usersRouter.post('/', (req, res) => {
+  /*
+    retrieve username and password from request body.
+    hash password using bcrypt and save new user to database.
+    create JWT token and send it back in response if successful,
+    or send error message if request fails.
+  */
+});
+
+// authenticate user and generate JWT token
+usersRouter.post('/login', (req, res) => {
+  /*
+    retrieve username and password from request body.
+    verify user exists in database and check password with bcrypt.
+    create JWT token and send it back in response if successful,
+    or send error message if request fails.
+  */
+});
+
+// get current user's data
+usersRouter.get('/me', (req, res) => {
+  /*
+    verify JWT token in authorization header.
+    find user in database based on decoded token.
+    send user data in response if successful,
+    or send error message if request fails or token is invalid.
+  */
+});
+
+// update current user's data
+usersRouter.put('/me', (req, res) => {
+  /*
+    verify JWT token in authorization header.
+    find user in database based on decoded token.
+    update user data and save to database.
+    send updated user object in response if successful,
+    or send error message if request fails or token is invalid.
+  */
+});
+
+// delete current user's account
+usersRouter.delete('/me', (req, res) => {
+  /*
+    verify JWT token in authorization header.
+    find user in database based on decoded token.
+    delete user from database.
+    send success message in response if successful,
+    or send error message if request fails or token is invalid.
+  */
+});
+
+module.exports = usersRouter;
