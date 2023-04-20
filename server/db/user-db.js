af851217c5f27ca6198bbb1d5b832f929fb38087
Harshi <lharshitharebala@gmail.com>
2023-04-20 13:50:10 -0700

    Functions for Users db

--- server/db/user-db.js
+++ server/db/user-db.js
@@ -11,30 +11,60 @@ const pool = mysql.createPool({
 
 
 // creates user 
-const createUser = (username, hash, salt) => {
-    // check if username exists in table
-    //if it does, error
-    //if it doesnt, insert row
-
-    /* example query (not sure if this is 100% right)
-    INSERT INTO users (username, salt, hash_password, date_created, last_login)
-    VALUES (username, salt, hash, CURRENT_DATE, NULL);
-    */
+const createUser = async (username, hash, salt) => {
+    try {
+        // Check if username exists in table
+        const query = 'SELECT username FROM users WHERE username = ?';
+        const result = await pool.query(query, [username]);
+        //checks if any rows were returned
+        if (result.length > 0) {
+          throw new Error('Username already exists'); 
+        } else  {
+            const result = await pool.query('INSERT INTO users (username, salt, hash_password, date_created, last_login) VALUES (?, ?, ?, CURRENT_DATE, NULL)', [username, salt, hash]);
+            console.log('User created successfully');
+        }
+    } catch (error) {
+        console.error(error);
+        connection.rollback();
+        return error;
+    } 
 }
 
 
 // get salt from db based on username
-const getSalt = (username) => {
-    /* example query (not sure if this is 100% right)
-    SELECT salt FROM users WHERE username = username;
-    */
+const getSalt = async (username) => {
+    try {
+        const query = 'SELECT salt FROM users WHERE username = ?';
+        const result = await pool.query(query, [username]);
+        const rows = result[0];
+        if (rows.length > 0) {
+            return rows[0].salt;
+        } else {
+            throw new Error('User not found');
+        }
+    } catch (error) {
+        console.error(error);
+        return error;
+    }
 }
 
 
 
-// boolean, checks if hash matches
-const authenticate = (username, hash) => {
-    /* example query (not sure if this is 100% right)
-    SELECT hash_password FROM users WHERE username = username;
-    */
+
+const authenticate = async (username, hash) => {
+    try {
+        // Check if username exists in table
+        const query = 'SELECT username FROM users WHERE username = ?';
+        const result = await pool.query(query, [username]);
+        if (result.length > 0) {
+            const user = result[0];
+            const savedHash = user.hash_password;
+            return hash === savedHash;
+        } else {
+            throw new Error('User not found');
+        }
+    } catch (error) {
+        console.error(error);
+        return error;
+    } 
 }
