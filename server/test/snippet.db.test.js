60f773c6fb467f75aa94b0b4acf8e7a1f9622e48
elimelt <elimelt@uw.edu>
2023-04-25 17:03:13 -0700

    passed all snippet db tests

--- server/test/snippet.db.test.js
+++ server/test/snippet.db.test.js
@@ -12,7 +12,7 @@ const {
 
 
 describe('getSnippetByType', () => {
-  it('should return a success response when query is successful', async () => {
+  it('should return the correct data', async () => {
     const snippet1 = {
       id: 6969,
       type: 'WHILE_LOOP',
@@ -34,45 +34,50 @@ describe('getSnippetByType', () => {
       data: ['THIS IS TESTING DATA', '\tSystem.out.println(i);']
     }
 
-    const snippets = [snippet1, snippet2, snippet3];
+    const createRes1 = await createSnippet(snippet1);
+    expect(createRes1.success).toBe(true);
+    const result1 = await getSnippetByType(snippet1.type);
 
-    await snippets.forEach(async (snippet, i) => {
-      const createRes = await createSnippet(snippet);
-      expect(createRes.success).toBe(true);
-      const result = await getSnippetByType(snippet.type);
+    expect(result1.length).toBe(1);
+    expect(result1[0].type).toBe("WHILE_LOOP");
 
-      console.log(result);
+    const createRes2 = await createSnippet(snippet2);
+    expect(createRes2.success).toBe(true);
+    const result2 = await getSnippetByType(snippet2.type);
 
-      expect(result.length).toBe(i + 1);
-      
-      expect(result[0].type).toBe("WHILE_LOOP");
-    })
+    expect(result2.length).toBe(2);
+    expect(result2[0].type).toBe("WHILE_LOOP");
+    
+    const createRes3 = await createSnippet(snippet3);
+    expect(createRes3.success).toBe(true);
+    const result3 = await getSnippetByType(snippet3.type);
 
-    await snippets.forEach(async snippet => {
-      const deleteRes = await deleteSnippetByID(snippet.id)
-      expect(deleteRes.success).toBe(true);
-    });
+    expect(result3.length).toBe(3);
+    expect(result3[0].type).toBe("WHILE_LOOP");
+    
+    const deleteRes1 = await deleteSnippetByID(snippet1.id)
+    expect(deleteRes1.success).toBe(true);
+
+    const deleteRes2 = await deleteSnippetByID(snippet2.id)
+    expect(deleteRes2.success).toBe(true);
+
+    const deleteRes3 = await deleteSnippetByID(snippet3.id)
+    expect(deleteRes3.success).toBe(true);
   });
 
   it('should return empty array when no matching snippet', async () => {
-    // arrange
     const type = 'fake-type';
 
-    // act
     const result = await getSnippetByType(type);
 
-    // assert
-    
+    expect(result.length).toBe(0);
   });
 
   it('should handle edge cases gracefully (return empty array)', async () => {
-    // arrange
     const type = null;
 
-    // act
     const result = await getSnippetByType(type);
 
-    // assert
     expect(result.length).toBe(0);
   });
 });
