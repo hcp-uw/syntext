1fda84ac84ce02edd8b0306ea0696932f08c4b54
PenguinTamer101 <shajian@uw.edu>
2023-04-29 13:46:39 -0700

    added login route

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
-import { Route, Routes } from 'react-router-dom'
-import Main from './Pages/Main'
-import AccountInfo from './Pages/AccountInfo'
+import { Route, Routes } from "react-router-dom";
+import Main from "./Pages/Main";
+import AccountInfo from "./Pages/AccountInfo";
+import Login from "./Pages/Login";
 
 const App = () => {
   return (
     // Any other pages we plan to add will go here
     <Routes>
-      <Route path='/' element={<Main />} />
-      <Route path='/account' element={<AccountInfo />} />
+      <Route path="/" element={<Main />} />
+      <Route path="/account" element={<AccountInfo />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/Pages/Login.js
@@ -0,0 +1,5 @@
+const AccountInfo = () => {
+  return <div className="login-container"></div>;
+};
+
+export default AccountInfo;
