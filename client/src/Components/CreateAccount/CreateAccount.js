f3b7f508bcda26ee2419758490f22ce04e279a78
elimelt <elimelt@uw.edu>
2023-05-09 15:46:03 -0700

    created signup page and integrated/styled CreateAccount component

--- client/src/Components/CreateAccount/CreateAccount.js
+++ client/src/Components/CreateAccount/CreateAccount.js
@@ -1,7 +1,12 @@
 import { useState } from 'react'
 import { createUser } from '../../services/userService'
+import './CreateAccount.css'
+import { useDispatch } from 'react-redux'
 
-const Login = () => {
+import { setLoggedIn, setUserID } from './../../redux/user/userStateActions';
+
+const CreateAccount = () => {
+  const dispatch = useDispatch(); 
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [errorMessage, setErrorMessage] = useState(null)
@@ -10,47 +15,58 @@ const Login = () => {
 
   const handlePasswordChange = event => setPassword(event.target.value)
 
-  const handleCreate = async event => {
+  const handleLogin = async event => {
     event.preventDefault()
     const createResult = await createUser(username, password)
     if (createResult.success) {
-      alert('created user ' + username)
+      alert('logged in as ' + username)
       setErrorMessage(null)
       window.localStorage.setItem('authToken', createResult.token)
+      dispatch(setLoggedIn(true))
     } else {
-      setErrorMessage('Username not available')
+      setErrorMessage('Invalid username or password')
     }
   }
 
   return (
-    <>
-      <h2>Create new account</h2>
-      <form onSubmit={handleCreate}>
-        <div>
-          <label>Username:</label>
+    <div className="create-container" style={{margin: "auto"}}>
+      <h2 className="create-title" style={{fontSize: "2em", color: "#7D615F"}}>create account</h2>
+
+      <form onSubmit={handleLogin}>
+        <div className="input-container">
           <input
+            className="input-textbox"
             type='text'
             id='username'
+            placeholder='username'
             value={username}
             onChange={handleUsernameChange}
           />
         </div>
-        <div>
-          <label>Password:</label>
+        <div className="input-container">
           <input
+            className="input-textbox"
             type='password'
             id='password'
+            placeholder='password'
             value={password}
             onChange={handlePasswordChange}
           />
         </div>
-        <button type='submit' onClick={handleCreate}>
-          Create
-        </button>
-        {errorMessage && <span style={{ color: 'red' }}>{errorMessage}</span>}
+        <div className='button-container'>
+          <button className="submit-button" type='submit' onClick={handleLogin}>
+            create
+          </button>
+        </div>
+        <div style={{ textAlign: 'center', paddingTop: '12px' }}>
+          {errorMessage && <span 
+              style={{ whiteSpace: "pre-line", 
+                       color: 'red' }}>{errorMessage}</span>}
+        </div>
       </form>
-    </>
+    </div>
   )
 }
 
-export default Login
+export default CreateAccount;
+
---
+++ client/src/Pages/SignUpPage.js
@@ -0,0 +1,12 @@
+import CreateAccount from '../Components/CreateAccount/CreateAccount'
+
+const SignUpPage = () => {
+    return (
+        <div className="page-container">
+            <CreateAccount/>
+        </div>
+    )
+}
+
+
+export default SignUpPage;
