c077828a7683327bbbdbd8be648b6c9658f7bc5d
elimelt <elimelt@uw.edu>
2023-05-09 15:45:30 -0700

    added button to NavBar for creating account, and conditional rendering of login/join button for whether or not user is logged in

--- client/src/Components/NavBar/NavBar.js
+++ client/src/Components/NavBar/NavBar.js
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
 import Icon from '../Icon/Icon';
 import './NavBar.css';
-
+import { useSelector } from 'react-redux';
 const NavBar = ({ setSettingsFocus }) => {
+
+  const isLoggedIn = useSelector(s => s.userState.isLoggedIn);
   return (
     <div className='navbar-container'>
       <div className='navbar'>
@@ -13,7 +15,8 @@ const NavBar = ({ setSettingsFocus }) => {
           <Link to='/leaderboard'>leaderboard</Link>
           <Link to='/'>game</Link>
           <Link to='/account'>account</Link>
-          <Link to='/login'>login</Link>
+          {!isLoggedIn && <Link to='/login'>login</Link>}
+          {!isLoggedIn && <Link to='/join'>join</Link>}
           <Icon setSettingsFocus={setSettingsFocus} />
         </div>
       </div>
