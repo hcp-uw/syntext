54eb55c1a0be86288b30c9fcbba92f0db98e1688
elimelt <elimelt@uw.edu>
2023-05-09 15:44:33 -0700

    created new route for signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import { useState } from 'react'
+import { useDispatch, useSelector } from "react-redux";
 import NavBar from './Components/NavBar/NavBar';
 import Main from "./Pages/Main";
 import AccountInfo from "./Pages/AccountInfo";
 import LoginPage from "./Pages/LoginPage";
+import SignUpPage from "./Pages/SignUpPage"
 import LeaderboardPage from "./Pages/LeaderboardPage";
 import PopUpController from "./Components/PopupController/PopUpController";
 
+
 const App = () => {
+  
   const [settingsFocus, setSettingsFocus] = useState(false);
   return (
     <div className="app-container">
@@ -15,7 +19,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/account" element={<AccountInfo />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/join" element={<SignUpPage/>} />
+        <Route path="/login" element={<LoginPage/>} />
         <Route path="/leaderboard" element={<LeaderboardPage/>}/>
       </Routes>
       <PopUpController
