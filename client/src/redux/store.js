00fb2f0fbe248811d11948dc89653e8caef46e0d
elimelt <elimelt@uw.edu>
2023-05-09 15:38:42 -0700

    created app store for global state

---
+++ client/src/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./rootReducer";
+
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== "production",
+});
+
+export default store;
