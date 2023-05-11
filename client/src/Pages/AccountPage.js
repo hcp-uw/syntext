f07f363a39d87def46c3972a047befc33b6b420f
elimelt <elimelt@uw.edu>
2023-05-10 18:46:29 -0700

    created AccountManager component that lets you sign out

--- client/src/Pages/AccountPage.js
+++ client/src/Pages/AccountPage.js
@@ -1,5 +1,21 @@
-const AccountInfo = () => {
-  return <div className='account-info-container'></div>
+import { useSelector } from "react-redux"
+import AccountManager from "../Components/AccountManager/AccountManager"
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+// import AccountInfo from './AccountPage';
+
+
+const AccountPage = () => {
+  const navigate = useNavigate();
+  let isLoggedIn = useSelector(s => s.userState.isLoggedIn);
+  useEffect(() => {
+    if (!isLoggedIn) navigate('/login')
+  }, [])
+  return (
+    <div className='account-info-container'>
+      <AccountManager/>
+    </div>
+  )
 }
 
-export default AccountInfo
+export default AccountPage
