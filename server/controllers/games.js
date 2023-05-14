d45f00f9790e711f1fbc3f418afd56aac02e3a50
elimelt <elimelt@uw.edu>
2023-05-14 16:18:09 -0700

    finished current games api routes

--- server/controllers/games.js
+++ server/controllers/games.js
@@ -10,30 +10,31 @@ const jsonParser = bodyParser.json();
 
 gameRouter.post('/create', jsonParser, async (req, res) => {
     const gameObject = extractGame(req.body);
+    if (gameObject.error) return res.status(400).send({success: false})
     const token = extractToken(req);
+
 
 
     try {
         const decodedToken = await verifyToken(token);
-        console.log(decodedToken)
-        console.log(gameObject)
+
         if (!decodedToken.success || gameObject.userID != decodedToken.userID)
             res.status(401).send({success: false, error: 'Token unauthorized'});
         const result = await createGameEntry(gameObject);
-        console.log(result)
+
         if (result.success)
             res.status(201).send({ success: true });
         else 
            return res.status(400).send({error: 'Bad Request: Missing data in request body'}); 
 
     } catch (error) {
-        console.log(error);
+        console.error(error);
         res.status(500).send("Internal Server Error");
     }
 });
 
 gameRouter.get('/games', jsonParser, async (req, res) => {
-    let {userID, username} = req.body;
+    let {userID, username} = req.query;
     if (!userID && !username) 
         return res.status(400).send({error: 'Bad Request: Missing data in request body'}); 
     
@@ -52,6 +53,7 @@ gameRouter.get('/games', jsonParser, async (req, res) => {
 gameRouter.delete('/games', jsonParser, async (req, res) => {
     let {userID, username} = req.body;
     const token = extractToken(req);
+    console.log('params1: ', userID, username)
 
     if (!userID && !username) 
         return res.status(400).send({error: 'Bad Request: Missing data in request body'}); 
@@ -59,6 +61,7 @@ gameRouter.delete('/games', jsonParser, async (req, res) => {
     if (!userID)
         userID = await getUserID(username);
 
+        console.log('params2: ', userID, username)
     const decodedToken = await verifyToken(token);
     
     if (!decodedToken.success || userID != decodedToken.userID)
@@ -66,6 +69,7 @@ gameRouter.delete('/games', jsonParser, async (req, res) => {
 
     try {
         const result = await clearGameEntries(userID);
+        console.log(result);
         if (result.success) res.status(200).send(result);
         else res.status(400)
     } catch (error) {
@@ -75,6 +79,8 @@ gameRouter.delete('/games', jsonParser, async (req, res) => {
 
 
 const extractGame = (body) => {
+    if (!body.data) return {error: true};
+    
     const {
         userID, 
         snippet_id,
@@ -84,14 +90,27 @@ const extractGame = (body) => {
         wpm_avg, 
         accuracy, 
         num_mistakes
-    } = body;
+    } = body.data;
+
+
+
+    if (!(
+        typeof userID === typeof 2 &&
+        typeof snippet_id === typeof 2 &&
+        typeof total_time === typeof 2 &&
+        typeof total_characters === typeof 2 && 
+        wpm_data &&
+        typeof wpm_avg === typeof 2.2 &&
+        typeof accuracy === typeof 2.2 &&
+        typeof num_mistakes === typeof 2
+        )) return {error: true};
 
     return {
         userID: userID,
         snippet_id:snippet_id,
         total_time : total_time,
         total_characters: total_characters, 
-        wpm_data: wpm_data,
+        wpm_data: wpm_data.toString(),
         wpm_avg: wpm_avg,
         accuracy: accuracy,
         num_mistakes: num_mistakes,
@@ -99,6 +118,14 @@ const extractGame = (body) => {
 } 
 
 const extractToken = (req) => {
+    if (
+        req.method === 'POST' &&
+        req.body !== undefined && 
+        req.body.headers !== undefined &&
+        req.body.headers.Authorization !== undefined
+    ) {
+        return req.body.headers.Authorization
+    }
     const tokenIndex =
     req.rawHeaders.findIndex(header => header === 'Authorization') + 1
     return req.rawHeaders[tokenIndex];
