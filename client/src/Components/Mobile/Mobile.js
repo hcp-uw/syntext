14546bf14c93707fb2980a6d3495fd2f681534b5
William Eng <1williameng@gmail.com>
2023-05-30 00:21:03 -0700

    mobile screen written- popup , text , and header
    Co-authored-by: Sriya Bulusu <sriyabulusu@users.noreply.github.com>

--- client/src/Components/Mobile/Mobile.js
+++ client/src/Components/Mobile/Mobile.js
@@ -6,9 +6,56 @@ const Mobile = () => {
         <p className='mobile-header'>
             Synte&gt;&lt;t
         </p>
+        <div className='popup'>
+            <div className='mobile-header2'>
+                Please visit our site on desktop!
+            </div>
+        </div>  
         <p className='mobile-body'>
-            Please visit our site on desktop!
+            At Syntext, we are dedicated to providing programmers with a minimalistic and user-friendly 
+            typing practice solution. Our mission is to help users enhance their typing skills effortlessly,
+            with little to no setup required.
         </p>
+        <p className='mobile-body'>
+            Designed specifically with programmers in mind, Syntext offers a seamless experience 
+            right from the moment you visit our website. Say goodbye to tedious setups and 
+            lengthy configurations - with Syntext, you can immediately dive into honing your typing skills.
+        </p>
+        <p className='mobile-body'>
+            One of the key features that sets Syntext apart is its real-time input validation. 
+            As you type, our system provides instant feedback, encouraging you to focus on 
+            both accuracy and speed. By practicing typing in a manner that prioritizes 
+            both precision and swiftness, you'll be able to master the art of coding efficiently.
+        </p>
+        <p className='mobile-body'>
+            After completing the code displayed on your screen, Syntext presents you with 
+            comprehensive metrics to give you a clear understanding of your overall performance. 
+            You can track your progress, measure your accuracy, and assess your typing speed, 
+            allowing you to continuously improve your skills.
+        </p>
+        <p className='mobile-body'>
+            At Syntext, we believe that design and user experience are paramount. 
+            Our website is meticulously crafted to provide a simple yet robust platform 
+            that is highly responsive and stable. We prioritize features that are intuitive and easy to use, 
+            ensuring that your typing practice is enjoyable and distraction-free.
+        </p>
+        <p className='mobile-body'>
+            Whether you are a beginner looking to boost your typing skills or an experienced programmer 
+            aiming to refine your speed, Syntext is here to accompany you on your journey. 
+            Join us today and embark on a seamless typing practice experience that caters specifically 
+            to your needs.
+        </p>
+        <p className='mobile-body'>
+            Start typing with Syntext (on desktop) and unlock your full coding potential!
+        </p>
+        <footer>
+            <div className='footer-container'>
+                Check out our Github:
+                <div>
+                    <a className='mobile-body' href="mailto:hege@example.com">hege@example.com</a>
+                </div>
+            </div>
+        </footer>
     </div>
         
     )
